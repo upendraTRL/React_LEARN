90800249b98a40cda16bbbc5d13a6e92971a5126
upendraTRL <upendrataral21@gmail.com>
2023-02-27 18:00:14 +0530

    Click event, Done.

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -6,11 +6,25 @@ const Content = () => {
         return names[int];
     }
 
+    const handleClick = () => {
+        console.log("You clicked it!");
+    }
+
+    const handleClick2 = (name) => {
+        console.log(`${name} was clicked!`);
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText);
+    }
+
     return (
         <main>
-            <p>
+            <p onDoubleClick={handleClick}>
                 Hello {handleTech()} !
             </p>
+            <button onClick={handleClick}>Click It!</button>
+            <button onClick={() => handleClick2("Upendra")}>Click It!</button>
+            <button onClick={(e) => handleClick3(e)}>Click It!</button>
         </main>
     )
 }
